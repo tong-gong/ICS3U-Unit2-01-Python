4e836790c10354474f881a1b7b1cb0c4fc9663cc
Ubuntu <ubuntu@ip-172-31-36-121.ec2.internal>
2020-11-24 18:10:12 +0000

    commit

---
+++ circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by Tong Gong
+# Created time November 2020
+# This is a program to calculate the area and the perimeter of a circle
+
+import math
+
+
+def main():
+    print("If a circle has a radious of 15mm")
+    print("")
+    print("The area is {}mm²".format(15*15*math.pi))
+    print("The perimeter is {}mm".format(2*15*math.pi))
+
+
+if __name__ == "__main__":
+    main()
